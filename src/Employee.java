dce3d169824760f726a21a0fd9368180cffe428c
YuriPetukhov <petuchowjuri@yahoo.de>
2023-04-21 17:46:12 +0300

    Term project 1. Повышенная сложность.

--- src/Employee.java
+++ src/Employee.java
@@ -4,7 +4,7 @@ class Employee {
     private String fullName;
     private int unitNumber;
     private double salary;
-    public static int id = 0;
+    public static int id;
 
     public Employee(String fullName, int unitNumber, double salary) {
         this.fullName = fullName;
@@ -49,13 +49,13 @@ class Employee {
 
     public static void printEmployeesList(String[] fullName, int[] unitNumber, double[] salary) {
         System.out.println("Список сотрудников:");
-        System.out.printf("| %s | %-30s | %5s | %9s |", "№", "ФИО", "Отдел", "Зарплата");
+        System.out.println("--------------------------------------------------------------------");
+        System.out.printf("| %s | %-40s | %5s | %9s |", "№", "ФИО", "Отдел", "Зарплата");
         System.out.println();
-        System.out.println("==========================================================");
+        System.out.println("====================================================================");
         for (int i = 0; i < id; i++) {
-            System.out.printf("| %d | %-30s | %5d | %.2f |", i + 1, fullName[i], unitNumber[i], salary[i]);
-            System.out.println();
-            System.out.println("----------------------------------------------------------");
+            System.out.printf("| %d | %-40s | %5d | %.2f |\n", i + 1, fullName[i], unitNumber[i], salary[i]);
+            System.out.println("--------------------------------------------------------------------");
         }
     }
 
@@ -64,6 +64,7 @@ class Employee {
         System.out.printf("Сумма затрат на зарплату в месяц составляет: %.2f рублей\n", costWages);
 
     }
+
     public static void printMinWageEmployee(String[] fullName, double[] salary) {
         double minWage = salary[0];
         int num = 0;
@@ -76,6 +77,7 @@ class Employee {
         System.out.printf("Минимальную зарплату %.2f рублей получает %s\n", minWage, fullName[num]);
 
     }
+
     public static void printMaxWageEmployee(String[] fullName, double[] salary) {
         double maxWage = salary[0];
         int num = 0;
@@ -88,15 +90,126 @@ class Employee {
         System.out.printf("Максимальную зарплату %.2f рублей получает %s\n", maxWage, fullName[num]);
 
     }
-    public static void printAAverageWages(double[] salary) {
+
+    public static void printAverageWages(double[] salary) {
         double costWages = Arrays.stream(salary).sum();
-        System.out.printf("Средняя зарплата в месяц составляет: %.2f рублей\n", costWages/id);
+        System.out.printf("Средняя зарплата в месяц составляет: %.2f рублей\n", costWages / id);
 
     }
+
     public static void printFullNames(String[] fullNames) {
         for (int i = 0; i < id; i++) {
             System.out.println(fullNames[i]);
         }
     }
 
+    public static void increaseWage(String[] fullNames, double[] salary, int percent) {
+        for (int i = 0; i < id; i++) {
+            salary[i] += salary[i] / 100 * percent;
+            System.out.printf("%s получает теперь %.2f рублей в месяц\n", fullNames[i], salary[i]);
+        }
+    }
+
+    public static void printMinWageUnit(String[] fullName, double[] salary, int[] unitNumber, int unit) {
+        double minWage = 200_000;
+        int element = 0;
+        for (int i = 0; i < id; i++) {
+            if (unitNumber[i] == unit && minWage > salary[i]) {
+                minWage = salary[i];
+                element = i;
+            }
+        }
+        System.out.printf("Минимальную зарплату %.2f рублей в отделе %d получает %s\n", minWage, unit, fullName[element]);
+    }
+
+    public static void printMaxWageUnit(String[] fullName, double[] salary, int[] unitNumbers, int unit) {
+        double maxWage = 0;
+        int element = 0;
+        for (int i = 0; i < id; i++) {
+            if (unitNumbers[i] == unit && maxWage < salary[i]) {
+                maxWage = salary[i];
+                element = i;
+            }
+        }
+        System.out.printf("Максимальную зарплату %.2f рублей в отделе %d получает %s\n", maxWage, unit, fullName[element]);
+    }
+
+    public static void printCostWagesUnit(double[] salary, int[] unitNumbers, int unit) {
+        double wageUnitCost = 0;
+        for (int i = 0; i < id; i++) {
+            if (unitNumbers[i] == unit) {
+                wageUnitCost += salary[i];
+            }
+        }
+        System.out.printf("Сумма затрат на зарплату в месяц в отделе %d составляет: %.2f рублей\n", unit, wageUnitCost);
+
+    }
+
+    public static void printAverageWageUnit(double[] salary, int[] unitNumbers, int unit) {
+        double wageUnitSum = 0;
+        int number = 0;
+        for (int i = 0; i < id; i++) {
+            if (unitNumbers[i] == unit) {
+                number++;
+                wageUnitSum += salary[i];
+            }
+        }
+        System.out.printf("Средняя зарплата в месяц в отделе %s составляет: %.2f рублей\n", unit, wageUnitSum / number);
+    }
+
+    public static void increaseWageUnit(String[] fullNames, double[] salary, int[] unitNumbers, int unit, int percent) {
+        for (int i = 0; i < id; i++) {
+            if (unitNumbers[i] == unit) {
+                salary[i] += salary[i] / 100 * percent;
+                System.out.printf("%s в отделе %s получает теперь %.2f рублей в месяц\n", fullNames[i], unit, salary[i]);
+            }
+        }
+    }
+
+    public static void printEmployeesUnit(String[] fullName, int[] unitNumbers, double[] salary, int unit) {
+        System.out.println("Список сотрудников отдела:");
+        System.out.println("-------------------------------------------------------------");
+        System.out.printf("| %s | %-40s | %9s |", "№", "ФИО", "Зарплата");
+        System.out.println();
+        System.out.println("============================================================");
+        int num = 1;
+        for (int i = 0; i < id; i++) {
+            if (unitNumbers[i] == unit) {
+                System.out.printf("| %d | %-40s | %.2f |", num, fullName[i], salary[i]);
+                System.out.println();
+                System.out.println("------------------------------------------------------------");
+                num++;
+            }
+        }
+    }
+
+    public static void employeesLowerSalaries(int[] idNum, String[] fullName, int[] unitNumbers, double[] salary, int salaryLevel) {
+        System.out.println("Список сотрудников с более низкой зарплатой:");
+        System.out.println("-------------------------------------------------------------");
+        System.out.printf("| %s | %-40s | %9s |", "ID", "ФИО", "Зарплата");
+        System.out.println();
+        System.out.println("============================================================");
+        for (int i = 0; i < id; i++) {
+            if (salary[i] < salaryLevel) {
+                System.out.printf("| %2d | %-40s | %.2f |\n", idNum[i], fullName[i], salary[i]);
+                System.out.println("-------------------------------------------------------------");
+            }
+        }
+
+    }
+
+    public static void employeesHigherSalaries(int[] idNum, String[] fullName, int[] unitNumbers, double[] salary, int salaryLevel) {
+        System.out.println("Список сотрудников с более высокой зарплатой:");
+        System.out.println("-------------------------------------------------------------");
+        System.out.printf("| %s | %-40s | %9s |", "ID", "ФИО", "Зарплата");
+        System.out.println();
+        System.out.println("============================================================");
+        for (int i = 0; i < id; i++) {
+            if (salary[i] > salaryLevel) {
+                System.out.printf("| %2d | %-40s | %.2f |\n", idNum[i], fullName[i], salary[i]);
+                System.out.println("-------------------------------------------------------------");
+            }
+        }
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,39 @@
 public class Main {
     public static void main(String[] args) {
 
-        String [] fullNames = new String [10];
-        int [] unitNumbers = new int [10];
-        double [] salaries = new double[10];
+        String[] fullNames = new String[10];
+        int[] unitNumbers = new int[10];
+        double[] salaries = new double[10];
+        int[] idNum = new int[10];
         Employee employee1 = new Employee("Иванов Иван Иванович", 1, 100_000);
         fullNames[0] = employee1.getFullName();
         unitNumbers[0] = employee1.getUnitNumber();
         salaries[0] = employee1.getSalary();
+        idNum[0] = Employee.id;
 
         Employee employee2 = new Employee("Петров Петр Петрович", 2, 120_000);
         fullNames[1] = employee2.getFullName();
         unitNumbers[1] = employee2.getUnitNumber();
         salaries[1] = employee2.getSalary();
+        idNum[1] = Employee.id;
 
-        Employee employee3 = new Employee("Валентинова Валентина", 3, 110_000);
+        Employee employee3 = new Employee("Валентинова Валентина Валентиновна", 3, 110_000);
         fullNames[2] = employee3.getFullName();
         unitNumbers[2] = employee3.getUnitNumber();
         salaries[2] = employee3.getSalary();
+        idNum[2] = Employee.id;
 
         Employee employee4 = new Employee("Евгеньева Евгения Евгеньевна", 4, 105_000);
         fullNames[3] = employee4.getFullName();
         unitNumbers[3] = employee4.getUnitNumber();
         salaries[3] = employee4.getSalary();
+        idNum[3] = Employee.id;
 
         Employee employee5 = new Employee("Маратов Марат Маратович", 5, 115_000);
         fullNames[4] = employee5.getFullName();
         unitNumbers[4] = employee5.getUnitNumber();
         salaries[4] = employee5.getSalary();
+        idNum[4] = Employee.id;
 
         System.out.println("Задача 1");
         Employee.printEmployeesList(fullNames, unitNumbers, salaries);
@@ -43,10 +49,46 @@ public class Main {
         Employee.printMaxWageEmployee(fullNames, salaries);
 
         System.out.println("Задача 5");
-        Employee.printAAverageWages(salaries);
+        Employee.printAverageWages(salaries);
 
         System.out.println("Задача 6");
         Employee.printFullNames(fullNames);
 
+        System.out.println("Задача повышенной сложности 1");
+        int percent = 20;
+        Employee.increaseWage(fullNames, salaries, percent);
+
+        System.out.println("Задача повышенной сложности 2");
+        int unitMin = 3;
+        Employee.printMinWageUnit(fullNames, salaries, unitNumbers, unitMin);
+
+        System.out.println("Задача повышенной сложности 3");
+        int unitMax = 4;
+        Employee.printMaxWageUnit(fullNames, salaries, unitNumbers, unitMax);
+
+        System.out.println("Задача повышенной сложности 4");
+        int unitSalaryCost = 3;
+        Employee.printCostWagesUnit(salaries, unitNumbers, unitSalaryCost);
+
+        System.out.println("Задача повышенной сложности 5");
+        int unitAverage = 1;
+        Employee.printAverageWageUnit(salaries, unitNumbers, unitAverage);
+
+        System.out.println("Задача повышенной сложности 6");
+        int unitIncrease = 1;
+        int unitPercent = 10;
+        Employee.increaseWageUnit(fullNames, salaries, unitNumbers, unitIncrease, unitPercent);
+
+        System.out.println("Задача повышенной сложности 7");
+        int unitInfo = 5;
+        Employee.printEmployeesUnit(fullNames, unitNumbers, salaries, unitInfo);
+
+        System.out.println("Задача повышенной сложности 8");
+        int salaryLevelMax = 140_000;
+        Employee.employeesLowerSalaries(idNum, fullNames, unitNumbers, salaries, salaryLevelMax);
+
+        System.out.println("Задача повышенной сложности 9");
+        int salaryLevelMin = 130_000;
+        Employee.employeesHigherSalaries(idNum, fullNames, unitNumbers, salaries, salaryLevelMin);
     }
 }
